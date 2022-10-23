b00eda0d391289b407250dd026825bfc30e881a4
wooseok123 <hosmimam@naver.com>
2022-10-24 00:20:44 +0900

    how to reply

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,27 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!"),
+  // .addStringOption((option) =>
+  //   option.setName("input").setDescription("The input to echo back")
+  // )
+  // .addChannelOption((option) =>
+  //   option.setName("channel").setDescription("The channel to echo into")
+  // ),
+
   async execute(interaction) {
-    await interaction.reply("Pong!");
+    await interaction.reply("마귀가 들렸네");
+
+    // await interaction.reply('Pong!');
+    // await wait(2000);
+    // await interaction.editReply('Pong again!');
+    // ephemeral => 본인만 확인할 수 있는 메시지 (남이 굳이 확인할 필요가 없는...)
+    //   await interaction.deferReply({ ephemeral: true });
+    //   await wait(4000);
+    //   // deferReply => 생각 중
+    //   // editReply => 이후에 답변 수정, deferReply를 쓰기 위해서는 이후 editReply로 수정해야 함
+    //   await interaction.editReply("Pong!");
+    //   await wait(2000);
+    //   await interaction.deleteReply();
+    //   await interaction.followUp({ content: "Pong again!", ephemeral: true });
   },
 };
--- index.js
+++ index.js
@@ -4,7 +4,22 @@ const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
 const { token } = require("./config.json");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-console.log(client.commands);
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, "commands");
@@ -24,30 +39,5 @@ for (const file of commandFiles) {
     );
   }
 }
-
-client.once(Events.ClientReady, () => {
-  console.log("Ready!");
-});
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
 
 client.login(token);
